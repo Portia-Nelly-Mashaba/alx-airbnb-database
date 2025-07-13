73ca4d0a3dffc75ebbfa4fe3258ba8fad12a0862
pnpnelly@gmail.com <Portia-Nelly-Mashaba>
2025-07-13 12:50:59 +0200

    updated Subquery aggregates

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -12,11 +12,15 @@ ORDER BY total_bookings DESC;
 
 -- Query 2: Rank properties by number of bookings (Window Function)
 SELECT 
-    Booking.property_id,
-    Property.name,
-    Property.location,
-    Property.pricepernight,
-    COUNT(*) OVER (PARTITION BY Booking.property_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(*) OVER (PARTITION BY Booking.property_id) DESC) AS booking_rank
-FROM Booking
-JOIN Property ON Booking.property_id = Property.property_id;
+    p.property_id,
+    p.name,
+    p.location,
+    p.pricepernight,
+    COUNT(b.booking_id) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+FROM Property p
+LEFT JOIN Booking b ON p.property_id = b.property_id
+GROUP BY p.property_id, p.name, p.location, p.pricepernight
+ORDER BY booking_rank;
+
+
